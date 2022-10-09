10869eff5a3f72761df8d1cfd03cc6c06f60c7a1
Alex Zubenko <o.zubenko@uni-bit.com>
2022-10-09 20:29:36 +0300

    Refactoring

--- Assets/Scripts/Network/NetworkInGameMessages.cs
+++ Assets/Scripts/Network/NetworkInGameMessages.cs
@@ -1,23 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
 public class NetworkInGameMessages : NetworkBehaviour
 {
     InGameMessagesUIHander inGameMessagesUIHander;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    
     public void SendInGameRPCMessage(string userNickName, string message)
     {
         RPC_InGameMessage($"<b>{userNickName}</b> {message}");
     }
-
 
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     void RPC_InGameMessage(string message, RpcInfo info = default)
